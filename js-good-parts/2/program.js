9e1395f55d34e2edaf4a5b35d729ab441daaa27a
Morgan Bauer <bauer.morgan@gmail.com>
2015-05-19 21:13:24 -0500

    ch2 notes and play code

---
+++ js-good-parts/2/program.js
@@ -0,0 +1,38 @@
+var that = this;
+
+
+/* a block comment */
+/*
+a
+multiline
+block
+comment
+*/
+// a comment for a single line
+
+3
+3.5
+3e-10
+3.5e6
+
+'string'
+"another string"
+
+var x = 'this';
+var y, z = 'that';
+var u = "the other thing", v, s, t;
+var a = "a", b = "b";
+var d, e ,f, g;
+
+{
+
+}
+
+
+here : {
+
+}
+
+there : {
+    var arst = 'arst';
+}
