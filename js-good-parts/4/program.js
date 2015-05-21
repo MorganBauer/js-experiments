9abb3e45c4baa7e1c2bd4708b53dc3e834c9c123
Morgan Bauer <bauer.morgan@gmail.com>
2015-05-20 23:45:51 -0500

    ch 4 functions

---
+++ js-good-parts/4/program.js
@@ -0,0 +1,76 @@
+document.writeln('Hello, World!');
+
+
+// shortcut to adding methods to things
+Function.prototype.method = function (name, func) {
+    this.prototype[name] = func;
+    return this;
+}
+ 
+
+
+var double = function (x)
+{
+    return x+x;
+}
+
+var fib = function (x)
+{
+    if(0 === x)
+    {
+	return 0;
+    }
+    if(1 === x)
+    {
+	return 1;
+    }
+    if(2 === x)
+    {
+	return 1;
+    }
+    return fib(x-1) + fib(x-2)
+}
+
+function add (x,y)
+{
+    return x + y;
+}
+
+function Quo (string) {
+    this.status = string;
+}
+
+Quo.prototype.get_status = function () {
+    return this.status;
+}
+
+new Quo('confusing');
+
+
+String.method('trim',function () {
+    return this.replace(/^\s+|\s+$/g, '');
+});
+
+
+
+// add conditionally
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name])
+    {
+	this.prototype[name] = func;
+	return this;
+    }
+}
+
+car myObj = (function () {
+    var value = 0;
+
+    return {
+	increment: function (inc) {
+	    value += typeof inc === 'number ' ? inc: 1;
+	},
+	getValue: function () {
+	    return value;
+	}
+    };
+}()); // this calls the function that was returned
